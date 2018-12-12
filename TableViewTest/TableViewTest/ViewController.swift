3991e623a9ca5462b896fb57c9c52033a6c779c0
Agarwal <rajatagarwal5@usblrrajatagamb.us.deloitte.com>
2018-12-12 11:59:45 +0530

    A Table view is Added

--- TableViewTest/TableViewTest/ViewController.swift
+++ TableViewTest/TableViewTest/ViewController.swift
@@ -18,3 +18,15 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController:UITableViewDataSource,UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
+
