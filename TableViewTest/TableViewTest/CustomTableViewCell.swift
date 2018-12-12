15ea4061276f6ad1352145bad5daa18d707b4380
Agarwal <rajatagarwal5@usblrrajatagamb.us.deloitte.com>
2018-12-12 14:40:12 +0530

    Json Data Model Created

---
+++ TableViewTest/TableViewTest/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  TableViewTest
+//
+//  Created by Agarwal, Rajat (US - Bengaluru) on 12/12/18.
+//  Copyright © 2018 Agarwal, Rajat (US - Bengaluru). All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ TableViewTest/TableViewTest/DataModel.swift
@@ -0,0 +1,9 @@
+//
+//  DataModel.swift
+//  TableViewTest
+//
+//  Created by Agarwal, Rajat (US - Bengaluru) on 12/12/18.
+//  Copyright © 2018 Agarwal, Rajat (US - Bengaluru). All rights reserved.
+//
+
+import Foundation
